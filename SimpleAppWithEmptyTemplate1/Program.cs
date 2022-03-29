820f7473f3a62a94fc00ee8aa48cc3db8061bdb2
Ella Li <v-ellali6@microsoft.com>
2022-03-29 01:06:30 -0700

    333

--- SimpleAppWithEmptyTemplate1/Program.cs
+++ SimpleAppWithEmptyTemplate1/Program.cs
@@ -1,6 +1,8 @@
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddRazorPages();
 var app = builder.Build();
 app.UseFileServer();
 //app.MapGet("/", () => "Hello World!");
-
+app.UseRouting();
+app.MapRazorPages();
 app.Run();
